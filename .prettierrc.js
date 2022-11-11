32da50e7bb568f05a6d8983390badcb65a80c742
tubring25 <tubring25@gmail.com>
2022-11-11 12:35:29 +0800

    🎉tada: init

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	printWidth: 80,
+	singleQuote: true,
+	trailingComma: 'all',
+	proseWrap: 'never',
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	quoteProps: 'as-needed',
+	bracketSpacing: true,
+	jsxSingleQuote: true,
+	arrowParens: 'avoid',
+	insertPragma: false,
+	requirePragma: false,
+	htmlWhitespaceSensitivity: 'css',
+	endOfLine: 'auto',
+	rangeStart: 0,
+};
