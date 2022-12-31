137f0ad6009515d842ef8a89e7d2fedcb3ccddb5
tubring25 <tubring25@gmail.com>
2022-12-31 21:34:30 +0900

    ✨feat: captcha

--- src/api/login/login.ts
+++ src/api/login/login.ts
@@ -1,9 +1,14 @@
-import { defHttp } from '@/service';
-
+import request from '@/service/request';
+// import axios from 'axios';
 enum Apis {
-  login = '/login',
+  captcha = '/micro-auth/captcha?key=777774396',
+  login = 'login',
 }
 
-export const login = () => {
-  return defHttp.get({ url: Apis.login });
+export const getCaptcha = (): Promise<any> => {
+  return request.get(Apis.captcha, { responseType: 'blob' });
+};
+
+export const login = (data: any): Promise<any> => {
+  return request.post(Apis.login, data);
 };
